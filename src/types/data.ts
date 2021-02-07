0299505427313eeedce2c59b38dbdaf592999d71
andras@newzoo.com <andras@newzoo.com>
2021-02-07 13:01:42 +0100

    added onMount fetch and logic to filter on selection

---
+++ src/types/data.ts
@@ -0,0 +1,11 @@
+export type TCategories = 'actors' | 'athletes' | 'comedians' | 'creators' | 'models' | 'musicians' | 'reality-tv'
+
+export interface ICelebrity {
+  categories: TCategories[]
+  id: string
+  name: string
+  price: number
+  rating: number
+  reviews: number
+  similar: string[]
+}
